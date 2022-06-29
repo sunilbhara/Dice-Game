40b1b5edf9c619eab745fbbe1c3d9067eab7bee6
Sunil Bharati <72141513+sunilbhara@users.noreply.github.com>
2022-06-29 04:47:00 -0700

    correct img path

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // For Dice 1
 var randomNumber1 = Math.floor((Math.random() * 6) + 1) ;
 
-var randomDiceImage1 = "/images/dice" + randomNumber1 + ".png";
+var randomDiceImage1 = "images/dice" + randomNumber1 + ".png";
 
 var image1 = document.querySelectorAll("img")[0];
 
@@ -11,7 +11,7 @@ image1.setAttribute("src", randomDiceImage1);
 // Foe Dice 2
 var randomNumber2 = Math.floor((Math.random() * 6) + 1) ;
 
-var randomDiceImage2 = "/images/dice" + randomNumber2 + ".png";
+var randomDiceImage2 = "images/dice" + randomNumber2 + ".png";
 
 document.querySelectorAll("img")[1].setAttribute("src", randomDiceImage2);
 
